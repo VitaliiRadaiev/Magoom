998065282dfa472057e2d01ccb006032786de3ea
Vitalii Radaiev <hf.chelenger@gmail.com>
2024-07-23 18:00:40 +0300

    add all pages

--- #src/js/vendors.js
+++
@@ -1,3 +0,0 @@
-@@include('plugins/swiper-bundle.js');
-@@include('plugins/gsap.js');
-@@include('plugins/scrollTrigger.js');
--- dist/js/app.js
+++ dist/js/app.js
@@ -638,6 +638,29 @@ function initCssVariableHeight() {
         window.addEventListener('resize', setVariable);
     })
 }
+function initCssVariableWidth() {
+    const elements = document.querySelectorAll('[data-css-variable-width]');
+    elements.forEach(el => {
+        const variableName = el.getAttribute('data-css-variable-width');
+        if (!variableName) return;
+
+        const parent = el.closest(el.getAttribute('data-parent'));
+
+        const setVariable = () => {
+            if(parent) {
+                parent.style.setProperty(variableName, el.clientWidth + 'px');
+            } else {
+                document.body.style.setProperty(variableName, el.clientWidth + 'px');
+            }
+        }
+
+        setVariable();
+
+        setInterval(setVariable, 200);
+
+        window.addEventListener('resize', setVariable);
+    })
+}
 
 function initParallax() {
     const elements = document.querySelectorAll('[data-scroll-parallax]');
@@ -699,83 +722,8 @@ window.addEventListener("DOMContentLoaded", () => {
 	initSmoothScrollByAnchors();
 	initAnchorsLinkOffset();
 	initCssVariableHeight();
+	initCssVariableWidth();
 	initParallax();
-
-
-	// ==== components =====================================================
-	{
-    const spoilers = document.querySelectorAll('[data-spoiler]');
-    if (spoilers.length) {
-        spoilers.forEach(spoiler => {
-            const swiper = spoiler.closest('[data-scroll-container]')?.swiper;
-            let isOneActiveItem = spoiler.dataset.spoiler.trim() === 'one' ? true : false;
-            let triggers = spoiler.querySelectorAll('[data-spoiler-trigger]');
-            if (triggers.length) {
-                triggers.forEach(trigger => {
-                    let parent = trigger.parentElement;
-                    let content = trigger.nextElementSibling;
-
-                    // init
-                    if (trigger.classList.contains('active')) {
-                        content.style.display = 'block';
-                        parent.classList.add('active');
-                    }
-
-                    trigger.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        parent.classList.toggle('active');
-                        trigger.classList.toggle('active');
-                        content && slideToggle(content, 300);
-                        swiper && setTimeout(() => swiper.update(), 300);
-
-                        if (isOneActiveItem) {
-                            triggers.forEach(i => {
-                                if (i === trigger) return;
-
-                                let parent = i.parentElement;
-                                let content = i.nextElementSibling;
-
-                                parent.classList.remove('active');
-                                i.classList.remove('active');
-                                content && slideUp(content, 300);
-                                swiper && setTimeout(() => swiper.update(), 300);
-                            })
-                        }
-                    })
-                })
-            }
-        })
-    }
-
-    const radioSpoilers = document.querySelectorAll('[data-radio-spoiler]');
-    radioSpoilers.forEach(radioSpoiler => {
-        let triggers = Array.from(radioSpoiler.querySelectorAll('[data-radio-trigger]'))
-            .map(el => {
-                return {
-                    wrapper: el,
-                    input: el.querySelector('input[type="radio"]')
-                }
-            });
-        
-        triggers.forEach(trigger => trigger.input?.checked && trigger.wrapper.nextElementSibling.style.setProperty('display', 'block'));
-
-        radioSpoiler.addEventListener('change', (e) => {
-            const isEventFromTrigger = triggers.find(t => t.input === e.target);
-            if(isEventFromTrigger) {
-                triggers.forEach(trigger => {
-                    if(trigger.input?.checked) {
-                        slideDown(trigger.wrapper.nextElementSibling, 300);
-                    } else {
-                        slideUp(trigger.wrapper.nextElementSibling, 300);
-                    }
-                })
-            }
-        })
-    })
-}
-	// ==== // components =====================================================
-
-
 
 	// ==== sections =====================================================
 	{
@@ -801,6 +749,15 @@ window.addEventListener("DOMContentLoaded", () => {
     const mobileMenu = document.querySelector('[data-mobile-menu]');
     if(mobileMenu) {
         const burger = document.querySelector('[data-action="toggle-show-mobile-menu"]');
+        const mobileMenuList = mobileMenu.querySelector('.mobile-menu__list');
+        const listItems = Array.from(mobileMenuList.children);
+
+        listItems.forEach(li => {
+            const link = li.firstElementChild;
+            if(link.nextElementSibling) {
+                link.setAttribute('data-spoiler-trigger', '');
+            }
+        })
 
         
         const setHeaderAsFixed = () => {
@@ -939,6 +896,81 @@ window.addEventListener("DOMContentLoaded", () => {
 //     }
 // }
 	// ==== // sections =====================================================
+
+	// ==== components =====================================================
+	{
+    const spoilers = document.querySelectorAll('[data-spoiler]');
+    if (spoilers.length) {
+        spoilers.forEach(spoiler => {
+            const swiper = spoiler.closest('[data-scroll-container]')?.swiper;
+            let isOneActiveItem = spoiler.dataset.spoiler.trim() === 'one' ? true : false;
+            let triggers = spoiler.querySelectorAll('[data-spoiler-trigger]');
+            if (triggers.length) {
+                triggers.forEach(trigger => {
+                    let parent = trigger.parentElement;
+                    let content = trigger.nextElementSibling;
+
+                    // init
+                    if (trigger.classList.contains('active')) {
+                        content.style.display = 'block';
+                        parent.classList.add('active');
+                    }
+
+                    trigger.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        parent.classList.toggle('active');
+                        trigger.classList.toggle('active');
+                        content && slideToggle(content, 300);
+                        swiper && setTimeout(() => swiper.update(), 300);
+
+                        if (isOneActiveItem) {
+                            triggers.forEach(i => {
+                                if (i === trigger) return;
+
+                                let parent = i.parentElement;
+                                let content = i.nextElementSibling;
+
+                                parent.classList.remove('active');
+                                i.classList.remove('active');
+                                content && slideUp(content, 300);
+                                swiper && setTimeout(() => swiper.update(), 300);
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    }
+
+    const radioSpoilers = document.querySelectorAll('[data-radio-spoiler]');
+    radioSpoilers.forEach(radioSpoiler => {
+        let triggers = Array.from(radioSpoiler.querySelectorAll('[data-radio-trigger]'))
+            .map(el => {
+                return {
+                    wrapper: el,
+                    input: el.querySelector('input[type="radio"]')
+                }
+            });
+        
+        triggers.forEach(trigger => trigger.input?.checked && trigger.wrapper.nextElementSibling.style.setProperty('display', 'block'));
+
+        radioSpoiler.addEventListener('change', (e) => {
+            const isEventFromTrigger = triggers.find(t => t.input === e.target);
+            if(isEventFromTrigger) {
+                triggers.forEach(trigger => {
+                    if(trigger.input?.checked) {
+                        slideDown(trigger.wrapper.nextElementSibling, 300);
+                    } else {
+                        slideUp(trigger.wrapper.nextElementSibling, 300);
+                    }
+                })
+            }
+        })
+    })
+}
+	// ==== // components =====================================================
+
+	
 
 	document.body.classList.add('page-loaded');
 });
